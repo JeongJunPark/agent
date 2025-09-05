f573f30440425842c4b3e2140b32a1d610203243
Kukvly <kukvly96@gmail.com>
2025-09-05 13:03:15 +0900

    에러 수정

--- src/mypage/PersonalInfoModify.js
+++ src/mypage/PersonalInfoModify.js
@@ -1,3 +1,5 @@
+import React, { useState, useEffect } from "react";
+import SendAPI from "../utils/SendAPI";
 const PersonalInfoModify = () => {
     useEffect(() => {
         SendAPI("https://home-api.leadcorp.co.kr:8080/updateManagerInfo", { ID: sessionStorage.getItem('ID'), menu: "개인정보수정", note: '', IP : sessionStorage.getItem('IP') })
